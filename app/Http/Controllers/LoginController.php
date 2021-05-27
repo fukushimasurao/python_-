4234d02e6afb0ad7415c8683978178cfb703179b
Hayato Fukushima <nu0640042@gmail.com>
2021-05-28 03:27:45 +0900

    環境変数の設定

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,6 +9,10 @@ class LoginController extends Controller
 {
     public function twitter()
     {
+        $twitter_api_key = getenv('TWITTER_API_KEY');
+        $twitter_api_secret_key = getenv('TWITTER_API_SECRET_KEY');
+        $twitter_access_token = getenv('TWITTER_ACCESS_TOKEN');
+        $twitter_access_token_secret = getenv('TWITTER_ACCESS_SECRET');
 
         $twitter = new TwitterOAuth($twitter_api_key, $twitter_api_secret_key, $twitter_access_token, $twitter_access_token_secret);
         $twitter->post("statuses/update", array("status" => "TEST Tweet."));
